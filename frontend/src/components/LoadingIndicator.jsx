bf2cccfbebd233ddd3df333594378ef0ca64d760
Vikas Sharma <vikasvssharma1@gmail.com>
2025-02-13 14:58:47 -0600

    finished website functionality

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,9 @@
+import '../styles/LOadingIndicator.css'
+
+const LoadingIndicator = () => {
+    return <div className='LoadingIndicator'>
+        <div className='loader'></div>
+    </div>
+}
+
+export default LoadingIndicator;
