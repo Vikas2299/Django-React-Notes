ee29b16274f4ebcd81c89fee1107fa08301276f8
Vikas Sharma <vikasvssharma1@gmail.com>
2025-02-20 14:21:11 -0600

    Add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 
+const apiURL = 'https://de710891-ec5d-4778-9ae9-6464aa1f3a11-dev.e1-us-east-azure.choreoapis.dev/djangoreactnotes/backend/v1';
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
+});
 
 api.interceptors.request.use(
     (config) => {
